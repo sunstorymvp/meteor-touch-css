fa550f9bdfbc89277dc09241da6a0df711096537
Dmitriy Haponov <d.haponov@111minutes.com>
2015-10-11 16:18:20 +0300

    update files structure

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'sunstory:meteor-touch-css',
   summary: 'Remove :hover style rules on touch devices.',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/sunstorymvp/meteor-touch-css.git'
 });
 
 Package.onUse(function(api) {
-  api.addFiles('client/meteor-touch-css.js', 'client');
+  api.addFiles('meteor-touch-css.js', 'client');
 });
